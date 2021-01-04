f34bc4700ce2cbb5692726bca80de9a1b7f23b09
BrunoSampaioDev <sampaionascimento100@gmail.com>
2021-01-03 21:42:02 -0300

    first commit

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,26 @@
+class Negociacao{
+
+    constructor(data, quantidade, valor){
+        this._data = new Date(data.getTime()); //garantindo data imutavel
+        this._quantidade = quantidade ;
+        this._valor = valor;
+        Object.freeze(this); //Congelando o this para que nao seja possivel altera-lo
+    }
+
+    get volume(){
+        return this._quantidade * this._valor;
+    }
+
+    get data(){
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade(){
+        return this._quantidade;
+    }
+
+    get valor(){
+        return this._valor;
+    }
+
+}
